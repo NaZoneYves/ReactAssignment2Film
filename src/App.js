70ed895236e582f7b725eb4b9c39b1e0c176c6f7
NaZoneYves <vngtngan@gmail.com>
2024-09-03 09:45:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TestApi from './components/testApi';
+import test2 from './components/test2.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <TestApi></TestApi>
     </div>
   );
 }
---
+++ src/components/testApi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestApi() {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  )
+}
